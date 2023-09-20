f619c1ebe2ffd0036f2aff0807cb6acf28984aee
shimilin <shumilin.i.v@gmail.com>
2023-09-20 14:28:56 +0300

    Добавляем статику

---
+++ pages/router.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+
+
+router = APIRouter(
+    prefix="",
+    tags=["Pages"],
+)
+
+templates = Jinja2Templates(directory="templates")
+@router.get("/base")
+def get_base_page(request: Request):
+    return templates.TemplateResponse("auth/base.html", {"request": request})
+
+@router.get("/register")
+def register(request: Request):
+    return templates.TemplateResponse("auth/register.html", {"request": request})
+
+@router.get("/recover-password")
+def recover_password(request: Request):
+    return templates.TemplateResponse("auth/recover-password.html", {"request": request})
+
+@router.get("/forgot-password")
+def forgot_password(request: Request):
+    return templates.TemplateResponse("auth/forgot-password.html", {"request": request})
