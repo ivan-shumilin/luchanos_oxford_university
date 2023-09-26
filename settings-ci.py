0d7d9a768cc03fca91754a06f310fb95820d0240
shumilin <shumilin.i.v@gmail.com>
2023-09-26 12:37:58 +0300

    fix

--- settings-ci.py
+++ settings-ci.py
@@ -7,14 +7,14 @@ REAL_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
     default="postgresql+asyncpg://postgres:postgres@37.140.195.68:5432/postgres",
 )  # connect string for the real database
-# APP_PORT = env.int("APP_PORT")
-APP_PORT = 8000
+APP_PORT = env.int("APP_PORT")
+# APP_PORT = 8000
 
 SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
 ALGORITHM: str = env.str("ALGORITHM", default="HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", default=300)
-# SENTRY_URL: str = env.str("SENTRY_URL")
-SENTRY_URL: str = ""
+SENTRY_URL: str = env.str("SENTRY_URL")
+# SENTRY_URL: str = ""
 
 # test envs
 TEST_DATABASE_URL = env.str(
