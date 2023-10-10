9d36de9528226e04a1610ae64301af2ee1872ac5
shimilin <shumilin.i.v@gmail.com>
2023-10-10 23:54:05 +0300

    отправляем все файлы

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ async def read_root(request: Request, db: AsyncSession = Depends(get_db)):
             #     logger.error(err)
     data = {
         'chat_id': obj.message.chat.id,
-        'text': "answer",
+        'text': answer,
         'reply_markup': json.dumps(reply_markup)
     }
 
